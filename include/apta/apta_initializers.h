b28b805f49cea68577298bedf391ccf6cd50c45a
Daniel Vučinović <58355393+dvucinozd@users.noreply.github.com>
2026-07-28 15:35:36 +0200

    feat(api): add output structure initializers

---
+++ include/apta/apta_initializers.h
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+#ifndef APTA_INITIALIZERS_H
+#define APTA_INITIALIZERS_H
+
+#include <apta/apta_config.h>
+#include <apta/apta_result.h>
+#include <apta/apta_source.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+APTA_API void APTA_CALL
+apta_frame_range_init(apta_frame_range_t *range);
+
+APTA_API void APTA_CALL
+apta_memory_requirements_init(apta_memory_requirements_t *requirements);
+
+APTA_API void APTA_CALL
+apta_progress_init(apta_progress_t *progress);
+
+APTA_API void APTA_CALL
+apta_request_progress_init(apta_request_progress_t *progress);
+
+APTA_API void APTA_CALL
+apta_result_info_init(apta_result_info_t *info);
+
+APTA_API void APTA_CALL
+apta_diagnostic_view_init(apta_diagnostic_view_t *view);
+
+APTA_API void APTA_CALL
+apta_waveform_overview_view_init(apta_waveform_overview_view_t *view);
+
+APTA_API void APTA_CALL
+apta_waveform_tile_view_init(apta_waveform_tile_view_t *view);
+
+APTA_API void APTA_CALL
+apta_tempo_view_init(apta_tempo_view_t *view);
+
+APTA_API void APTA_CALL
+apta_grid_view_init(apta_grid_view_t *view);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* APTA_INITIALIZERS_H */
