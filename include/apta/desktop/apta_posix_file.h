1a938da3867e24b193f60d9461c722e6be556f9b
Daniel Vučinović <58355393+dvucinozd@users.noreply.github.com>
2026-07-29 01:54:42 +0200

    feat(s5): add POSIX file adapter API

---
+++ include/apta/desktop/apta_posix_file.h
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+#ifndef APTA_DESKTOP_POSIX_FILE_H
+#define APTA_DESKTOP_POSIX_FILE_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <apta/apta_errors.h>
+#include <apta/apta_types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct apta_posix_file apta_posix_file_t;
+
+APTA_API apta_status_t APTA_CALL
+apta_posix_file_open_read(
+    const char *path,
+    apta_posix_file_t **file_out);
+
+APTA_API apta_status_t APTA_CALL
+apta_posix_file_get_size(
+    const apta_posix_file_t *file,
+    uint64_t *size_out);
+
+APTA_API apta_status_t APTA_CALL
+apta_posix_file_read_at(
+    apta_posix_file_t *file,
+    uint64_t offset,
+    void *buffer,
+    size_t requested_bytes,
+    size_t *read_bytes_out);
+
+APTA_API void APTA_CALL
+apta_posix_file_close(apta_posix_file_t *file);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* APTA_DESKTOP_POSIX_FILE_H */
