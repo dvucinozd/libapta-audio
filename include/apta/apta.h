f95ecb54a7d166e57a1d2e9eac005a989f1ce2ab
Daniel Vučinović <58355393+dvucinozd@users.noreply.github.com>
2026-07-28 14:59:57 +0200

    feat(api): add umbrella public header

---
+++ include/apta/apta.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+#ifndef APTA_H
+#define APTA_H
+
+#include <apta/apta_version.h>
+#include <apta/apta_errors.h>
+#include <apta/apta_types.h>
+#include <apta/apta_config.h>
+#include <apta/apta_source.h>
+#include <apta/apta_result.h>
+
+#endif /* APTA_H */
