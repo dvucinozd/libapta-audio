a6ad0d7e9e440bf019f2f361859d4e9b21b7069f
Daniel Vučinović <58355393+dvucinozd@users.noreply.github.com>
2026-07-28 21:07:12 +0200

    feat(memory): define session workspace allocator interface

---
+++ src/core/apta_session_workspace.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Apache-2.0
+#ifndef APTA_SESSION_WORKSPACE_H
+#define APTA_SESSION_WORKSPACE_H
+
+#include "apta_internal.h"
+
+size_t apta_internal_session_workspace_minimum_size(void);
+
+int apta_internal_session_uses_workspace(
+    const apta_session_t *session);
+
+apta_status_t apta_internal_session_workspace_initialize(
+    apta_session_t *session);
+
+void *apta_internal_session_allocate(
+    apta_session_t *session,
+    size_t size,
+    size_t alignment,
+    apta_memory_flags_t flags);
+
+void apta_internal_session_deallocate(
+    apta_session_t *session,
+    void *memory);
+
+#endif /* APTA_SESSION_WORKSPACE_H */
