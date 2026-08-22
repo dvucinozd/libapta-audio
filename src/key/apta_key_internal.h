43bea45750f6f7984e7a73082694a318eba48f9b
Daniel Vučinović <58355393+dvucinozd@users.noreply.github.com>
2026-08-22 09:20:32 +0200

    Place key state in core session layout

--- src/key/apta_key_internal.h
+++ src/key/apta_key_internal.h
@@ -5,33 +5,6 @@
 #include "../core/apta_internal.h"
 #include "../core/apta_result_pool.h"
 
-#define APTA_INTERNAL_KEY_OCTAVES 3u
-#define APTA_INTERNAL_KEY_PITCH_CLASSES 12u
-#define APTA_INTERNAL_KEY_BIN_COUNT \
-    (APTA_INTERNAL_KEY_OCTAVES * APTA_INTERNAL_KEY_PITCH_CLASSES)
-#define APTA_INTERNAL_KEY_CANDIDATE_COUNT 3u
-#define APTA_INTERNAL_KEY_DECIMATION 4u
-#define APTA_INTERNAL_KEY_STABLE_WINDOWS 4u
-#define APTA_INTERNAL_KEY_PUBLISH_INTERVAL 4u
-
-typedef struct {
-    float coefficients[APTA_INTERNAL_KEY_BIN_COUNT];
-    float q1[APTA_INTERNAL_KEY_BIN_COUNT];
-    float q2[APTA_INTERNAL_KEY_BIN_COUNT];
-    float chroma[APTA_INTERNAL_KEY_PITCH_CLASSES];
-    float decimation_sum;
-    uint32_t decimation_count;
-    uint32_t window_samples;
-    uint32_t window_target_samples;
-    uint32_t completed_windows;
-    uint32_t selected_windows;
-    apta_source_frame_t evidence_first_frame;
-    apta_source_frame_t evidence_end_frame;
-    apta_source_frame_t next_source_frame;
-    uint32_t has_next_source_frame;
-    uint32_t initialized;
-} apta_internal_key_analysis_t;
-
 void apta_internal_key_feed_sample(
     apta_session_t *session,
     float sample,
